8a95e15042bd4620050bce6581d075b881662923
akashsarker99 <akashsarker2003@gmail.com>
2026-06-22 12:09:26 +0600

    project server created and rquired packages installed and mongodb connected

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Server is running!");
+})
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const run = async () => {
+    try {
+        await client.connect();
+       const db = client.db('donora-project');
+
+        await client.db("admin").command({ ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`);
+})
