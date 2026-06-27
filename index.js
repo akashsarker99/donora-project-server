526fc1175e77a16a2c5c6d5dc959f0bdc560e45d
akashsarker99 <akashsarker2003@gmail.com>
2026-06-27 11:25:12 +0600

    temporary removed jwt verification to avoid error

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 
 const app = express();
@@ -25,27 +24,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-const JWKS = createRemoteJWKSet(
-    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
-);
-
-const verifyToken = async (req, res, next) =>{
-  const authHeader = req?.headers.authorization
-  if(!authHeader){
-    return res.status(401).json({message: "Unauthorized"})
-  }
-  const token = authHeader.split(" ")[1]
-    if(!token){
-    return res.status(401).json({message: "Unauthorized"})
-  }
-   try {
-    const {payload} = await jwtVerify(token, JWKS)
-    req.user = payload
-    next();
-   } catch (error) {
-      res.status(403).json({message: "Forbidden"})
-   }
-}
 const run = async () => {
     try {
         await client.connect();
@@ -54,7 +32,7 @@ const run = async () => {
        const requestCollection = db.collection('requests')
        const userCollection = db.collection('user');
        
-      app.post('/users',verifyToken, async (req, res) => {
+      app.post('/users', async (req, res) => {
       const user = req.body;
        const existingUser = await userCollection.findOne({email: user.email});     
     if (existingUser) {
@@ -78,7 +56,7 @@ const run = async () => {
   res.json(result);
 });
 
-         app.get("/users",verifyToken, async (req, res) => {
+         app.get("/users", async (req, res) => {
             const { email } = req.query;
 
           if (email) {
@@ -88,23 +66,23 @@ const run = async () => {
         const users = await userCollection.find().toArray();
          res.json(users);
         });
-        app.patch('/users/:email',verifyToken, async(req, res)=>{
+        app.patch('/users/:email', async(req, res)=>{
             const {email} = req.params;
             const data = req.body;
             const result = await userCollection.updateOne({ email }, { $set: {...data, updatedAt: new Date()} });
             res.json(result);
         })
         
-        app.get('/payment',verifyToken, async(req, res) =>{
+        app.get('/payment', async(req, res) =>{
             const result = await paymentCollection.find().toArray();
             res.json(result);
         })
-        app.post('/payment',verifyToken, async(req, res)=>{
+        app.post('/payment', async(req, res)=>{
             const data = req.body;
             const result = await paymentCollection.insertOne({...data, createdAt: new Date()});
             res.json(result);
         })
-        app.get('/request',verifyToken, async(req, res) =>{
+        app.get('/request', async(req, res) =>{
             const status = req.query.status;
             const email = req.query.email;
             const query = {}
@@ -117,23 +95,23 @@ const run = async () => {
             const result = await requestCollection.find(query).toArray();
             res.json(result);
         })
-        app.post('/request',verifyToken, async(req, res)=>{
+        app.post('/request', async(req, res)=>{
             const data = req.body;
             const result = await requestCollection.insertOne({...data, createdAt: new Date()});
             res.json(result);
         })
-        app.get('/request/:id',verifyToken, async(req, res) =>{
+        app.get('/request/:id', async(req, res) =>{
             const {id} = req.params;
             const result = await requestCollection.findOne({_id: new ObjectId(id)});
             res.json(result || {});
         })
-        app.patch('/request/:id',verifyToken,  async(req, res) =>{
+        app.patch('/request/:id',  async(req, res) =>{
             const {id} = req.params;
             const data = req.body;
             const result = await requestCollection.updateOne({_id: new ObjectId(id)}, {$set: data});
             res.json(result);
         })
-        app.delete('/request/:id',verifyToken, async (req, res) => {
+        app.delete('/request/:id', async (req, res) => {
             const { id } = req.params;
             const result = await requestCollection.deleteOne({_id: new ObjectId(id)});
             res.json(result);
@@ -158,6 +136,15 @@ const run = async () => {
             console.log(query)
             res.json(result);
 });
+
+        app.get("/stats", async (req, res) => {
+            const totalDonors = await userCollection.countDocuments({role: "donor",status: "active"});
+            const activeRequests = await requestCollection.countDocuments({requestStatus: "inprogress"});
+             const payments = await paymentCollection.find().toArray();
+            const totalFunding = payments.reduce((sum, payment) => sum + Number(payment.amount),0);
+
+            res.json({totalDonors,activeRequests,totalFunding});
+        });
 
         await client.db("admin").command({ ping: 1});
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
